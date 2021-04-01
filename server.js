9a58e605b1ee1c25af7db1f78633f1268343195c
Bjornvdb <bjorn.vandebergen@student.ucll.be>
2021-04-01 10:48:12 +0200

    fix 2

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ app.use(express.static('./public'));
 
 // Start server
 app.listen(9090, () => {
-    console.log('Listening on http://localhost:9090');
+    console.log('Listening on http://localhost:9090 bla');
 });
 
